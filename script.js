8481534e8e4fa7fa26af4238c428265a798a4645
FahadAlHossain <fahadalhossain5@gmail.com>
2024-12-02 18:42:07 +0600

    quote generate and tweet done

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
 const api_url = 'https://dummyjson.com/quotes/random';
+const quote = document.getElementById("quote");
+const author = document.getElementById("author");
 
 const getQuote = async (url) =>{
     const res = await fetch(url);
     const data = await res.json();
-    console.log(data);
+    quote.innerHTML = data.quote;
+    author.innerHTML = data.author;
 }
 
+const quoteBtn = () => {
+    getQuote(api_url);
+}
+
+const tweetBtn = () => {
+    window.open(`https://twitter.com/intent/tweet?text=${quote.innerHTML} ----- by ${author.innerHTML}`, "Tweet Window", "width:600, height:300")
+}
 getQuote(api_url);
